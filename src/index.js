80d9023b970a17fe811e42d83f1cdab39aa8a148
Vitali Kulsha <73823615+vitalikulsha@users.noreply.github.com>
2022-06-28 14:22:39 +0300

    feat: implement function check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = Array.from(str);
+  const closeBrackets = [];
+  for(let i = 0; i < arr.length; i++){
+    for(let k = 0; k < bracketsConfig.length; k++){         
+      if(arr[i] === bracketsConfig[k][0] && !closeBrackets.includes(arr[i])){
+        closeBrackets.push(bracketsConfig[k][1]); 
+        break;       
+      }      
+      if(arr[i] === bracketsConfig[k][1]){
+        if(arr[i] === closeBrackets[closeBrackets.length - 1]){
+          closeBrackets.pop();
+          break;
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  if(closeBrackets.length > 0){
+    return false;
+  }
+  return true;
 }
